2ccd940695cae02fab07b5e8a0e453a1a351e30e
Sruthi-Palle <pallesruthi12@gmail.com>
2022-12-20 14:02:24 +0530

    created Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className="flex sm:flex-row flex-col bg-gradient-to-r from-fuchsia-200 to-cyan-200 h-[700px] items-center justify-center sm:justify-around ">
+      <p className="text-fuchsia-800 p-4 text-center font-extralight sm:text-xl">
+        Hi I am
+        <span className="block text-4xl sm:text-5xl font-light">
+          Sruthi, a front-end developer
+        </span>{" "}
+        with a passion to design and build user-interfaces
+      </p>
+      <img
+        src="https://i.ibb.co/VCkkWxT/8530-E800-530-C-4-B32-BC4-B-988093897853-1-201-a.jpg"
+        alt="8530-E800-530-C-4-B32-BC4-B-988093897853-1-201-a"
+        className="rounded-t-full h-[450px] sm:h-[500px]"
+      />
+    </div>
+  );
+};
